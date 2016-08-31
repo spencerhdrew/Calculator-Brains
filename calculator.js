8f14e81236dfefe097810911eeff8d68fb9be003
Spencer H. Drew <spencerhdrew@Spencers-MacBook-Pro.local>
2016-08-30 21:11:10 -0700

    test test

--- calculator.js
+++ calculator.js
@@ -1,14 +1,23 @@
 var calculator = {
-		sum: 0,
-		add: function(value) {
-      this.sum += value;
-    },
-    clear: function() {
-      this.sum = 0;
-    }, 
-    equals: function() {
-      return this.sum;
-    }
+  sum: 0,
+  add: function(value) {
+    this.sum += value;
+  },
+  subtract: function(value) {
+  	this.sum -=value;
+  },
+  multiply: function(value) {
+  	this.sum *=value;
+  },
+  divide: function(value) {
+  	this.sum /=value;
+  },
+  clear: function() {
+    this.sum = 0;
+  },
+  equals: function() {
+    return this.sum;
+  }
 }
 
 //what happens when I made a change to this file?
